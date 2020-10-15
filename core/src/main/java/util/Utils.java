75e9e95add82254ea5fd2196b8dcf6f17388ffb6
Evelina Sarkisyan <evelina.sarkisyan.1990@mail.ru>
2020-10-16 01:32:02 +0300

    changes

---
+++ core/src/main/java/util/Utils.java
@@ -0,0 +1,17 @@
+package util;
+
+import utils.StringUtils;
+
+public class Utils {
+
+    public static boolean isAllPositiveNumbers(String... str){
+
+        for(int i = 0; i < str.length; i++){
+            if(!StringUtils.isPositiveNumber(str[i])){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
